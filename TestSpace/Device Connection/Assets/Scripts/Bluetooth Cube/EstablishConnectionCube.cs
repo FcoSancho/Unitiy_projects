dd8bbbdc1541ec4491dad47ac339b03fbee0db90
Francisco Sancho <60715437+FcoSancho@users.noreply.github.com>
2021-02-16 20:47:10 -0600

    Updated

---
+++ TestSpace/Device Connection/Assets/Scripts/Bluetooth Cube/EstablishConnectionCube.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+#if ENABLE_WINMD_SUPPORT
+using Windows.Devices.Bluetooth.Advertisement;
+#endif
+
+public class EstablishConnectionCube : MonoBehaviour {
+#if ENABLE_WINMD_SUPPORT
+
+    private readonly BluetoothLEAdvertisementWatcher mainBWatcher = new BluetoothLEAdvertisementWatcher {ScanningMode = BluetoothLEScanningMode.Active};
+    private bool listeningToDevice => mainBWatcher.Status == BluetoothLEAdvertisementWatcherStatus.Started;
+
+    private void Start() {
+        Application.OpenURL("yes");
+        mainBWatcher.Received += watcherReceived;
+        mainBWatcher.Stopped += watcherStopped;
+    }
+
+    private void watcherReceived(BluetoothLEAdvertisementWatcher yes, BluetoothLEAdvertisementReceivedEventArgs no) {}
+
+    private void watcherStopped(BluetoothLEAdvertisementWatcher yes, BluetoothLEAdvertisementWatcherStoppedEventArgs no) {}
+
+#endif
+}
